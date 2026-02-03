ee92fee455d0663cb531c53ec90561029cd6c66b
Albert Kinte <egliseneoapostolique1863@gmail.com>
2026-02-03 09:43:46 +0100

    update site

--- admin/admin-config.js
+++ admin/admin-config.js
@@ -1,6 +1,6 @@
 /**
- * Configuration accès admin – Seul ce(s) Gmail peut/peuvent se connecter.
+ * Configuration accès admin – S  eul ce(s) Gmail peut/peuvent se connecter.
  * Voir admin-config.example.js pour les instructions (Client ID Google, etc.)
  */
 window.ADMIN_ALLOWED_EMAIL = 'albertkintsodiza@gmail.com';
-window.ADMIN_GOOGLE_CLIENT_ID = '';  // Collez ici votre Client ID (voir admin-config.example.js)
+window.ADMIN_GOOGLE_CLIENT_ID = '794837147806-5eqnrj82pu4darqpngcrkrfr7sgo43f5.apps.googleusercontent.com';// Collez ici votre Client ID (voir admin-config.example.js)
